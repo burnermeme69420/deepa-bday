e83df4c3357e46b872f76081c5642aa9a298367c
adithyagenie <adithyagenie@gmail.com>
2024-05-22 20:12:39 +0530

    Almost done

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 document.getElementById('birthdayButton').addEventListener('click', function() {
     var imgContainer = document.getElementById('imageContainer');
+    var video = document.getElementById('backgroundVideo');
     imgContainer.innerHTML = ''; // Clear previous images if any
 
     // List of images
-    var images = ["assets/img1.jpg", "assets/img2.jpg", "assets/img3.jpg"];
-    // Create an image element and set attributes for each image
-    images.forEach(function(src) {
-        var img = document.createElement('img');
-        img.src = src;
-        img.style.display = 'inline-block'; // Display images side by side
-        imgContainer.appendChild(img);
-    });
+    var images = ["assets/img1.jpeg", "assets/img2.jpeg", "assets/img3.jpeg", "assets/img4.jpeg", "assets/img5.jpeg"];
 
-    // Change background after clicking the button
-    document.body.style.backgroundImage = "url('assets/bg.webp')";
+    // Hide the button after it's clicked
+    video.style.display = 'block';
+    this.style.display = 'none';
+    
+    setTimeout(() => {
+        // Create an image element and set attributes for each image
+        images.forEach(function(src) {
+            var img = document.createElement('img');
+            img.src = src;
+            img.style.display = 'inline-block'; // Display images side by side
+            imgContainer.appendChild(img);
+        });
+    }, 2000)
 });
